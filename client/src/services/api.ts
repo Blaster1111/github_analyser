9e04ca87c00dfd8f212510aed0a65d89b7f181aa
Blaster1111 <rudrashukla0711@gmail.com>
2025-02-27 22:50:36 +0530

    backend deployed

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://github-analyser-7t8e.onrender.com/api';
 
 export const analyzeRepo = async (repo_url: string) => {
     return axios.post(`${API_BASE_URL}/analyze-repo`, { repo_url });
