4df30e3ed82f5894edb0ad8a13832d6544d63915
Blaster1111 <rudrashukla0711@gmail.com>
2025-02-27 22:57:51 +0530

    minor changes

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -15,7 +15,7 @@ export const Navbar = () => {
                             <svg className="h-8 w-8 text-purple-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path>
                             </svg>
-                            <span className="ml-2 font-semibold text-xl text-gray-100">Repalyzer</span>
+                            <span className="ml-2 font-semibold text-xl text-gray-100">Gitlyser</span>
                         </Link>
                         <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
                             <Link
