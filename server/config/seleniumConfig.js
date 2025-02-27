71cc94beedfdadc479ed778072a5e08bc47a205a
Blaster1111 <rudrashukla0711@gmail.com>
2025-02-27 23:44:49 +0530

    selenium changed

--- server/config/seleniumConfig.js
+++ server/config/seleniumConfig.js
@@ -1,11 +1,21 @@
 import { Builder } from 'selenium-webdriver';
 import chrome from "selenium-webdriver/chrome.js";
 
-
 export const setupDriver = async () => {
     const options = new chrome.Options();
-    options.addArguments('--headless', '--no-sandbox', '--disable-dev-shm-usage');
-
+    
+    options.addArguments(
+        '--headless',
+        '--no-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu',
+        '--window-size=1920,1080', 
+        '--start-maximized',       
+        '--disable-extensions',    
+        '--disable-infobars'        
+    );
+    options.addArguments('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.6943.141 Safari/537.36');
+    
     return new Builder()
         .forBrowser('chrome')
         .setChromeOptions(options)
