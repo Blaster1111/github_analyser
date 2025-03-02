db14604fb82b3c3f31ad6a82bb70822a15fb51de
Blaster1111 <rudrashukla0711@gmail.com>
2025-03-02 12:11:16 +0530

    redployed render instance

--- fastApi/main.py
+++ fastApi/main.py
@@ -98,4 +98,5 @@ def scrape_gitingest(request: RepoRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
