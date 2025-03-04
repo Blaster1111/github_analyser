d6f153c28d9b1efd58486891bfed848958ca1674
Blaster1111 <rudrashukla0711@gmail.com>
2025-03-04 13:37:29 +0530

    removed cors attr

--- server/server.js
+++ server/server.js
@@ -10,8 +10,7 @@ app.use(express.json());
 
 app.use(cors({
     origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
 
 // Routes
